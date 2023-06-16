8f2e7bc5d4da7c152d35dcd71b56bffa58817991
Rafael Lopes <rafael.lopes@scora.com.br>
2023-06-16 17:33:35 -0300

    feat: factories de casos de uso + autenticação jwt

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config';
-import {z} from 'zod';
+import { z } from 'zod';
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 });
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from 'fastify';
-import { register } from './controlers/register';
 import { authenticate } from './controlers/authenticate';
+import { profile } from './controlers/profile';
+import { register } from './controlers/register';
+import { verifyJWT } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
-    
+
   app.post('/sessions', authenticate);
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
